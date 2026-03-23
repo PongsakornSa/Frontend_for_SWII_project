ced50824496163a1eb57106c79f5c2cfd091d897
Captain802 <poorisorn80@gmail.com>
2026-03-23 21:50:22 +0700

    add picture

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Car } from "@/types";
 
 export default function CarCard({ car, canEdit }: { car: Car; canEdit?: boolean }) {
   return (
     <div className="card">
-      <div className="space-between">
-        <h3 style={{ margin: 0 }}>{car.name}</h3>
+      
+      <div className="space-between" style={{ alignItems: "center" }}>
+        
+        <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+          <Image
+            src={`/${car.name}.png`}
+            alt={car.name}
+            width={80}
+            height={80}
+            style={{ objectFit: "cover", borderRadius: "8px" }}
+          />
+          <h3 style={{ margin: 0 }}>{car.name}</h3>
+        </div>
+
         <span className="badge">฿{car.pricePerHour}/hour</span>
       </div>
 
